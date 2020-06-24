c82d0324ee31ad3ba3bca6603d4f25fe47d626a1
dengqinghua <dengqinghua.42@gmail.com>
2020-06-25 00:26:00 +0800

    add

--- assets/javascripts/load_docs.js
+++ assets/javascripts/load_docs.js
@@ -9,7 +9,7 @@ $(function(){
       // $(this).append("<embed src='https://drive.google.com/viewerng/viewer?embedded=true&url=" + url + "' style='width:100%; height:650px;'>");
       var url = "/javascripts/pdf.js/web/viewer.html?file=" + host + "/doc/" + docName + ""
       // $(this).append("<embed src= " + url + " style='width:100%; height:1000px;'>");
-      $(this).append("<a href=" + url + " target='_blank'>"+ "PDF: " + docName + "</a>");
+      $(this).append("<a href=" + url + " target='_blank'>"+ docName + "</a>");
     }
   })
 })
